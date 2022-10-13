3e2aa2a3e2436d983c6ea6e09a055d8e52f19c15
jetfi <jordanpaigediedrich@gmail.com>
2022-10-13 18:28:25 -0500

    Initial commit

---
+++ app/src/main/java/com/example/myapplicationchat/MainActivity.kt
@@ -0,0 +1,81 @@
+package com.example.myapplicationchat
+
+import android.content.Intent
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import android.view.Menu
+import android.view.MenuItem
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.myapplicationchat.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.*
+
+class MainActivity : AppCompatActivity()
+{
+    private lateinit var userRecyclerView: RecyclerView
+    private lateinit var userList: ArrayList<User>
+    private lateinit var adapter: UserAdapter
+    private lateinit var mAuth: FirebaseAuth
+    private lateinit var mDbRef: DatabaseReference
+
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        mAuth = FirebaseAuth.getInstance()
+        mDbRef = FirebaseDatabase.getInstance().getReference()
+
+        userList = ArrayList()
+        adapter = UserAdapter(this, userList)
+
+        userRecyclerView = findViewById(R.id.userRecyclerView)
+
+        userRecyclerView.layoutManager = LinearLayoutManager(this)
+        userRecyclerView.adapter = adapter
+
+        mDbRef.child("user").addValueEventListener(object: ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot)
+            {
+                userList.clear()
+                for(postSnapshot in snapshot.children)
+                {
+                    val currentUser = postSnapshot.getValue(User::class.java)
+
+                    if (mAuth.currentUser?.uid != currentUser?.uid)
+                    {
+                        userList.add(currentUser!!)
+                    }
+                }
+                adapter.notifyDataSetChanged()
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+
+        })
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId == R.id.logout){
+            mAuth.signOut()
+            val intent = Intent(this@MainActivity, Login::class.java)
+            startActivity(intent)
+            return true
+        }
+        return true
+    }
+
+}
